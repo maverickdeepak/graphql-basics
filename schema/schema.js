09c247999b68f8154cd75a0fdd09d2f4c9908e2a
Deepak Chauhan <imdeepakchauhan12@gmail.com>
2024-01-06 16:46:27 +0530

    basic setup

---
+++ schema/schema.js
@@ -0,0 +1,56 @@
+const graphql = require("graphql");
+const axios = require("axios")
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphql;
+
+
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: {
+        id: {
+            type: GraphQLInt
+        },
+        name: {
+            type: GraphQLString
+        },
+        email: {
+            type: GraphQLString
+        }
+    }
+});
+
+const Company = new GraphQLObjectType({
+    name: "Company",
+    fields: {
+        id: {
+            type: GraphQLInt
+        },
+        name: {
+            type: GraphQLString
+        }
+    }
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/users/${args.id}`).then(res => res.data)
+            }
+        },
+        company: {
+            type: Company
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
